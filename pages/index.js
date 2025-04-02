3a3b9a2da07c5af60d7494a23599b4530e463522
Gabriel Hernandez <gabhernandez@hotmail.ca>
2025-04-02 00:00:55 -0400

    Updated index.js with Navbar, Footer, and FAQ layout. Installed Next.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head"
-import Chatbox from '../components/chatbox';
-import styles from '../styles/Home.module.css';
+import Chatbox from "../components/chatbox";
+import Navbar from "../components/navbar";
+import Footer from "../components/footer";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
